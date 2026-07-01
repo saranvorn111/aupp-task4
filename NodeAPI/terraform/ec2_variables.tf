16b0ac9954c0dc611becdd889469635c5e737ddd
Vorn saran <v.saran@dgc.gov.kh>
2026-07-02 00:50:39 +0700

    Refactor EC2 configuration; remove SSH key pair resource and associated private key generation

--- NodeAPI/terraform/ec2_variables.tf
+++ NodeAPI/terraform/ec2_variables.tf
@@ -1,13 +1,3 @@
-resource "tls_private_key" "ssh_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_key_pair" "deployer" {
-  key_name   = "mykeypair"
-  public_key = tls_private_key.ssh_key.public_key_openssh
-}
-
 resource "aws_instance" "web_server" {
 
   ami           = "ami-0ec10929233384c7f"
